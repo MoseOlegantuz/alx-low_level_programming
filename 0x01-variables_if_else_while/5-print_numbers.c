2b8ce53d2d183b279e7c6918125643aa69187f51
MoseOlegantuz <moseolegantuz@gmail.com>
2022-06-04 06:51:58 -0700

    whitespacesclear

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@
 */
 int main(void)
 {
-	int digit;
-	for (digit = 0; digit < 10; digit++)
+int digit;
+for (digit = 0; digit < 10; digit++)
 	printf("%i", digit);
 	putchar('\n');
-	return (0);
+return (0);
 }
