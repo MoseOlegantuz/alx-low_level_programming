bd5f2050ac91b32e66094d8d6a29c7b95b0e4f7e
Mose_Olegantuz <74275604+MoseOlegantuz@users.noreply.github.com>
2022-06-07 01:58:06 +0300

    Create 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main - entry point
- *
- * Return: 0
- *
- */
-=======
 * main - entry point
 *
 * Return: 0
 *
 */
->>>>>>> 87eb5dd1f1ee62f2f0f106596f608c536a54a808
 int main(void)
 {
-char a;
+	char a;
 
-<<<<<<< HEAD
-for (a = 'a'; a <= 'z'; a++)
-{
-if (a == 'q' || a == 'e')
-{
-continue;
-}
-putchar(a);
-=======
 	for (a = 'a'; a <= 'z' ; a++)
 	{
-	if (a == 'q' || a == 'e')
+		if (a == 'q' || a == 'e')
 		{
-		continue;
+			continue;
 		}
-	putchar(a);
+		putchar(a);
 	}
 	putchar('\n');
 	return (0);
->>>>>>> 87eb5dd1f1ee62f2f0f106596f608c536a54a808
 }
-putchar('\n');
-return (0);
-}
-
