743a4611c7b133415bea592e226e92c18601614b
MoseOlegantuz <moseolegantuz@gmail.com>
2022-06-13 22:58:06 +0300

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap values of two integers
+ * @x: int pointer type
+ * @y: int pointer type
+ */
+
+void swap_int(int *x, int *y)
+{
+	int z;
+
+	z = *x;
+	*x = *y;
+	*y = z;
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - function to reverse a string
+ * @s: char array of string type
+ */
+
+void rev_string(char *s)
+{
+	int i, c, m;
+	char x;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	m = i;
+	for (i--, c = 0; c < m / 2; i--, c++)
+	{
+		x = s[c];
+		s[c] = s[i];
+		s[i] = x;
+	}
+}
