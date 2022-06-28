d8d323c7e5031e5e3f5685de1acd96123979b359
MoseOlegantuz <moseolegantuz@gmail.com>
2022-06-28 19:23:11 +0300

    advanced

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: No return
+ */
+void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
